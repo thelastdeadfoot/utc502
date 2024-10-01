e816cb8b5b6aec14388845fa2ca38a3c363e4e01
thelastdeadfoot <152289728+thelastdeadfoot@users.noreply.github.com>
2024-10-01 12:39:30 +0200

    Update findme.sh

--- FindMe/findme.sh
+++ FindMe/findme.sh
@@ -6,6 +6,8 @@ findFile="findme.txt"
 for dir in "${path[@]}"; do
     echo "Recherche dans le repertoire '$dir'"
     for file in $(find "$dir" -type f -name "$findFile"); do
+      proprio=$(stat -c '%U' "$file")
+      echo "Le propriétaire de ce fichier était '$proprio'"
       rm -v "$file"
     done
 done
